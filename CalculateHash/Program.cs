07b234c958f8be7d5068fafac4a1e7ae0360e0c9
Fraysa <fraysati@gmail.com>
2015-07-03 16:49:31 +0300

    Added CalculateHash

---
+++ CalculateHash/Program.cs
@@ -0,0 +1,22 @@
+﻿using CalculateHash.Utilities;
+using System;
+
+namespace CalculateHash
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			if (args.Length == 1)
+			{
+				string path = args[0];
+				string hash = HashUtilities.GetMD5HashFromFile(path);
+
+				Console.WriteLine("Hash: {0}", hash);
+			}
+
+			Console.WriteLine("Press any key to quit...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ CalculateHash/Utilities/HashUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CalculateHash.Utilities
+{
+	internal static class HashUtilities
+	{
+		public static string GetMD5HashFromFile(string path)
+		{
+			FileStream file = new FileStream(path, FileMode.Open);
+			MD5 md5 = new MD5CryptoServiceProvider();
+			byte[] retVal = md5.ComputeHash(file);
+			file.Close();
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 0; i < retVal.Length; i++)
+			{
+				sb.Append(retVal[i].ToString("X2"));
+			}
+
+			return sb.ToString();
+		}
+	}
+}
